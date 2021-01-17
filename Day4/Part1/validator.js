115775ae6467e842ddb996d8eaa0d44622db1ef6
Mariano Baffetti <marianobaffetti@gmail.com>
2021-01-17 01:25:53 -0300

    Fixed methods sintaxis.

--- Day4/Part1/validator.js
+++ Day4/Part1/validator.js
@@ -4,15 +4,36 @@ class Validator {
     return parseInt(iyr) !== NaN;
   }
 
-  issueYearIsValid = (iyr) => {
+  issueYearIsValid (iyr) {
     return this.isANumber(iyr) && iyr.length == 4 && (iyr > 2010 && iyr < 2020);
   }
 
-  birthYearIsValid = (byr) => {
+  birthYearIsValid (byr) {
     return this.isANumber(byr) && byr.length == 4 && (byr > 1920 && byr < 2002);
   }
 
-  passportIsValid = (passport) => {
+  parsePassport (passportAsString) {
+    const pasportAsArray = passportAsString.replace(/\n/g, ' ').split(' ');
+
+    return pasportAsArray.reduce((acum, current) => {
+      let keyValue = current.split(':');
+      acum[keyValue[0]] = keyValue[1];
+      return acum;
+    }, {})
+  }
+
+  getValidPassports (input) {
+    return this.getPassportsAsString(input).reduce((acum, current) => {
+      this.passportIsValid(this.parsePassport(current)) && acum++;
+      return acum;
+    }, 0);
+  }
+
+  getPassportsAsString (passports) {
+    return passports.split('\n\n');
+  }
+
+  passportIsValid (passport) {
     if (!passport.byr)
       return false;
     if (!passport.iyr)
@@ -28,27 +49,6 @@ class Validator {
     if (!passport.pid)
       return false;
     return true;
-  }
-
-  parsePassport = (passportAsString) => {
-    const pasportAsArray = passportAsString.replace(/\n/g, ' ').split(' ');
-
-    return pasportAsArray.reduce((acum, current) => {
-      let keyValue = current.split(':');
-      acum[keyValue[0]] = keyValue[1];
-      return acum;
-    }, {})
-  }
-
-  getValidPassports = (input) => {
-    return this.getPassportsAsString(input).reduce((acum, current) => {
-      this.passportIsValid(this.parsePassport(current)) && acum++;
-      return acum;
-    }, 0);
-  }
-
-  getPassportsAsString = (passports) => {
-    return passports.split('\n\n');
   }
 }
 module.exports = Validator;
